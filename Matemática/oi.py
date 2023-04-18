ae2f27e8c786a9f1a1afedb3ac100c74a7a78493
Ícaro Kuchanovicz <icarolima.k7@gmail.com>
2023-04-18 20:41:46 -0300

    Update oi.py

--- Matemática/oi.py
+++ Matemática/oi.py
@@ -1,4 +1,47 @@
-print(2**128)
+from random import randint, choice
 
-print('18.446.744.073.709.551.616')
-print('340.282.366.920.938.463.463.374.607.431.768.211.456')
+num = [0,1,2,3,4,5,6,7,8,9]
+alphaP = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+alphaG = ['A', 'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T', 'U','V','W','X','Y','Z']
+non = ['*', '!', '#', '$', '%', '&', '+', '-', '/', ':', ';', '=', '?', '@','|']
+tud = [num, alphaP, alphaG, non]
+paw = [ ]
+
+qtd = int(input('Quantidade de caracteres [8-15]: '))
+
+c = 0
+while True:
+    paw.append(choice(choice(tud)))
+    print('oi')
+    if len(paw) == qtd:
+        for c in range(len(num)):
+            if num[c] in paw:
+                pass
+            else:
+                print('tem nao kkkk')
+                paw = [ ]
+
+        for c in range(len(alphaG)):
+            if alphaG[c] in paw:
+                pass
+            else:
+                paw = [ ]
+
+        for c in range(len(alphaP)):
+            if alphaP[c] in paw:
+                pass
+            else:
+                paw = [ ]
+
+        for c in range(len(non)):
+            if non[c] in paw:
+                pass
+            else:
+                paw = [ ]
+        break
+
+    c += 1
+    if c == 100:
+        break
+
+print(paw)
