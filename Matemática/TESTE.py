afd64f3d2a1436c30b7dc479fa783e6b97e33bed
Ícaro Kuchanovicz <icarolima.k7@gmail.com>
2023-04-19 10:47:08 -0300

    oi

--- Matemática/TESTE.py
+++ Matemática/TESTE.py
@@ -1,8 +1,14 @@
+#  A senha deve ter mínimo 8 e máximo 15 caracteres.
+#  A senha deve ter pelo menos uma letra maiúscula.
+#  A senha deve ter pelo menos uma letra minúscula.
+#  A senha deve ter pelo menos um símbolo. Ex: * ! # $ % & + - / : ; = ? @ \ |
+#  A senha deve ter pelo menos um número.
 from random import randint, choice
 
-a = [1,2,3,4,5]
-b = ['a', 'b', 'c','d', 'e']
-
-e = [a, b]
-
-print(choice(e)[0])
+numeros = [0,1,2,3,4,5,6,7,8,9]
+alphaP = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+alphaG = ['A', 'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T', 'U','V','W','X','Y','Z']
+especial = ['*', '!', '#', '$', '%', '&', '+', '-', '/', ':', ';', '=', '?', '@','|']
+opcoes = [numeros, alphaP, alphaG, especial]
+senha = [ ]
+qtd = int(input('Quantidade de caracteres [8-15]: '))
