248d03a614a431a0570ed0f25567ebb2f7631cfb
Ícaro Kuchanovicz <icarolima.k7@gmail.com>
2023-05-10 07:56:50 -0300

    Update EX11 escrito errado cpa.py

--- Raciocínio algorítmo/Exercícios Funções/EX11 escrito errado cpa.py
+++ Raciocínio algorítmo/Exercícios Funções/EX11 escrito errado cpa.py
@@ -1,11 +1,2 @@
 #Construa uma função que receba uma data no formato DD/MM/AAAA e devolva uma string no formato D de mesPorExtenso de AAAA. Opcionalmente, valide a data e retorne NULL caso a data seja inválida
 
-
-def data(dia,mes,ano):
-    oi
-
-
-# ta escrito tudo errado?
-
-
-
