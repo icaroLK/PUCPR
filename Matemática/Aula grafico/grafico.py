8b8cc1fd6d5f52c6848549fad7ae213e8f3eff02
Ícaro Kuchanovicz <icarolima.k7@gmail.com>
2023-05-18 11:33:57 -0300

    Create grafico.py

---
+++ Matemática/Aula grafico/grafico.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+# função:y = 2x + 1
+vetorX = np.arange(-10,10,0.1)
+#vetorY = [1,3,5,7,9,11]
+
+for x in vetorX:
+    vetorY.append(x)
+
+
+
+
+
+
+
+
+
+
+# criar uma figura
+fig = plt.figure(figsize=(5,5))
+plt.show()
+
+
