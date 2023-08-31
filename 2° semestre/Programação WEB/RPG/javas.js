fda2942e28aa586fbfaaa6f88cc0aedde15f60d8
Ícaro Kuchanovicz <icarolima.k7@gmail.com>
2023-08-31 11:22:51 -0300

    t

---
+++ 2° semestre/Programação WEB/RPG/javas.js
@@ -0,0 +1,11 @@
+const up = document.getElementById('up');
+const right = document.getElementById('right');
+const left = document.getElementById('left');
+const down = document.getElementById('down');
+
+function joystick(opa){
+var botao = document.getElementById(opa)
+window.alert("voce apertou   " + opa)
+}
+
+KeyboardEvent
