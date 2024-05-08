397c32d3894a87e1f2c9733d4aa4ce0a2d361d0f
Ícaro Kuchanovicz <icarolima.k7@gmail.com>
2024-05-08 10:51:41 -0300

    oi

---
+++ 3° semestre/POO/CURSOEMVIDEO-JAVA/src/Caneta.java
@@ -0,0 +1,20 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void rabiscar(){
+
+    }
+
+    void tampar(){
+
+    }
+
+    void destampar(){
+
+    }
+
+}
